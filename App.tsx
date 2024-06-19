1e4d47f051ba054e0fe6d8e3b65e088c0bcfab5f
Elvis Ronald Cruz Chullo <elviscruz45@Elviss-MacBook-Pro.local>
2024-06-18 19:53:38 -0500

    microphone configured

--- App.tsx
+++ App.tsx
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useVoiceRecognition } from "./hooks/useVoiceRecognition";
 
 export default function App() {
+  const [borderColor, setBorderColor] = useState<"lightgray" | "lightgreen">(
+    "lightgray"
+  );
+  const { state, startRecognizing, stopRecognizing, destroyRecognizer } =
+    useVoiceRecognition();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 32, fontWeight: "bold", marginBottom: 30 }}>
+        Talk GPT 🤖
+      </Text>
+      <Text style={styles.instructions}>{JSON.stringify(state, null, 2)}</Text>
+      <Text>
+        Press and hold this button to record your voice. Release the button to
+        send the recording, and you'll hear a response
+      </Text>
+
+      <Text style={styles.welcome}>Your message:</Text>
+
+      {/* <Text style={styles.welcome}>Your message: "{state.results[0]}"</Text> */}
+      {/* <Pressable
+        onPressIn={() => {
+          setBorderColor("lightgreen");
+          startRecognizing();
+        }}
+        onPressOut={() => {
+          setBorderColor("lightgray");
+          stopRecognizing();
+          handleSubmit();
+        }}
+        style={{
+          width: "90%",
+          padding: 30,
+          gap: 10,
+          borderWidth: 3,
+          alignItems: "center",
+          borderRadius: 10,
+          borderColor: borderColor,
+        }}
+      >
+        <Text style={styles.welcome}>
+          {state.isRecording ? "Release to Send" : "Hold to Speak"}
+        </Text>
+        <Image style={styles.button} source={require("./assets/button.png")} />
+      </Pressable>*/}
+      <Pressable
+        onPressIn={() => {
+          setBorderColor("lightgreen");
+          startRecognizing();
+        }}
+        onPressOut={() => {
+          setBorderColor("lightgray");
+          stopRecognizing();
+          // handleSubmit();
+        }}
+        style={{
+          width: "90%",
+          padding: 30,
+          gap: 10,
+          borderWidth: 3,
+          alignItems: "center",
+          borderRadius: 10,
+          borderColor: borderColor,
+        }}
+      >
+        <Text>Hold to speak</Text>
+      </Pressable>
+      <Button
+        title="Replay last message"
+        onPress={() => {}}
+
+        // onPress={async () => await playFromPath(urlPath)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    padding: 20,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  action: {
+    textAlign: "center",
+    color: "#0000FF",
+    marginVertical: 5,
+    fontWeight: "bold",
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
+    fontSize: 20,
+  },
+  stat: {
+    textAlign: "center",
+    color: "#B0171F",
+    marginBottom: 1,
   },
 });
